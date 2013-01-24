53b6bbde59db9fb4d124b401fe9bdd1e2509fb57
Ashwin Balamohan <abmohan@ashwins-macbook.(none)>
2013-01-23 23:22:12 -0500

    Added words to use as test words

--- amuthaa/test/test_TamilWord.py
+++ amuthaa/test/test_TamilWord.py
@@ -8,6 +8,51 @@ import unittest
 from amuthaa.TamilLetter import TamilLetter
 from amuthaa.TamilWord import TamilWord
 
+WORDS = (
+         {
+          'text': u"மரம்",
+          'len': 3,
+          'letters': (u'ம', u'ர', u'ம்'),
+          'syllables': (u'ம', u'ரம்')
+          },
+         {
+          'text': u"மாமரம்",
+          'len': 4,
+          'letters': (u'மா', u'ம', u'ரம்'),
+          'syllables': (u'மா', u'ம', u'ரம்')
+          },
+         {
+          'text': u"ஈ",
+          'len': 1,
+          'letters': (u'ஈ'),
+          'syllables': (u'ஈ')
+          },
+         {
+          'text': u"கை",
+          'len': 1,
+          'letters': (u"கை"),
+          'syllables': (u"கை")
+          },
+         {
+          'text': u"",
+          'len': 1,
+          'letters': (u'வ்'),
+          'syllables': (u'வ்')
+          },
+         {
+          'text': u"அரசியல்",
+          'len': 5,
+          'letters': (u'அ', u'ர', u'சி', u'ய', u'ல்'),
+          'syllables': (u'அ', u'ர', u'சி', u'யல்')
+          },
+         {
+          'text': u"பேனை",
+          'len': 2,
+          'letters': (u'பே', u'னை'),
+          'syllables': (u'பே', u'னை')
+          }
+         )
+
 
 class TamilWordTest(unittest.TestCase):
     """ A test class for the TamilWord module """
